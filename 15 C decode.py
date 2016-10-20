6531296c352c4027385fb4bb50e19545427333db
eAUE <kra.aue@gmail.com>
2016-10-20 12:50:30 -0400

    Support for non-alphabetic characters
    As stated beforehand, support for non-alphabetic characters was added in
    this version and testes.

--- 15 C decode.py
+++ 15 C decode.py
@@ -2,30 +2,24 @@ letterGoodness= [.0817, .0149, .0278, .0425, .1270, .0223, .0202, .0609, .0697, 
 print(letterGoodness) #testing... it's predefined
 inputo= str(input("Insert your encoded word: "))
 def ChrReplace(character, loopNo): #get the character and the probabilities of it being another character
-   if character == ' ': 
-      return ' '
-   if character == '\'':
-       return '\''
+   if ord(character) < 65 or ord(character) > 90:
+       return character
    ChrNum= ord(character) #Get the chr num
    newChrno= ChrNum + loopNo
    if newChrno > 90:
        newChrno = newChrno-26 #We account for if we go over z.
    return chr(newChrno) # Return the chr num.
 def probs(string): #Make a function that find the probability of each string character
-   if string == " ":
+   if ord(string) < 65 or ord(string) > 90:
        return 0
-   if string== '\'':
-        return 0
    alphaNo= ord(string) -65 #Get the number of it in the alphabet.
    probability= letterGoodness[alphaNo]
    return float(probability)
 def bigFunction(encodedString):
    encodedString= encodedString.upper()
    inputo= encodedString
-   newStr= ''
    goodnessList= []
    wordList= []
-   probabilityNewStr= 0
    for a in range (0, 26): # Loop around all 26 characters of the alphabet.
       newStr = ''
       probabilityNewStr = 0
@@ -38,4 +32,4 @@ def bigFunction(encodedString):
    location= goodnessList.index(biggestProbs)
    bigprobsWord= wordList[location]
    return bigprobsWord
-print(bigFunction(inputo)) #The beginning and the end of all
+print("The actual word is:", bigFunction(inputo)) #The beginning and the end of all
