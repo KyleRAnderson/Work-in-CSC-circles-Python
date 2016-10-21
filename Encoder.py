0954e5c032d79f1ed6510d5a5f3917eaae8a6c60
eAUE <kra.aue@gmail.com>
2016-10-21 16:39:48 -0400

    More support Added support for non-alphabetic characters. The code now, just like 15 C decode.py returns the encoded message in the same casing as the original message.

--- Encoder.py
+++ Encoder.py
@@ -14,10 +14,37 @@ def charactershifter(chrt, shiftvalue):
          No += (26* c)
    No= chr(No)
    return No
+# Code below is the same as in 15 C decode.py. Copy-pasted in fact. Adds more convenience to encoding.
+def characterReturn(newStr, ynList):
+    for a in range (0, len(newStr)):
+        if ynList[a] == 1: # 1= yes, 0= no (binary)
+            letterStr= newStr[a]
+            letterStr= letterStr.lower()
+            newStr= newStr[0: a] + letterStr + newStr[a+1:len(newStr)]
+            continue
+    return newStr
+def ynLister(string):
+    ynList= []
+    for a in range (0, len(string)):
+        individChr= string[a]
+        if individChr.isalpha()== False: ynList.append(0)
+        elif individChr.islower() == True: ynList.append(1)
+        else: ynList.append(0)
+    return ynList
 def encoder(message, shiftvalue):
    newStr= ''
+   ynList= ynLister(message)
+   if shiftvalue.isdigit() != True:
+         return ("I\'m sorry, that\'s not a valid shift value. Please try another number. The value must be an integer.")
+   else:
+         shiftvalue= int(shiftvalue)
    for a in range(0, len(message)):
-      newChr= charactershifter(message[a], shiftvalue)
-      newStr += newChr
+      if message[a].isalpha() == False:
+            newStr += message[a]
+            continue
+      else:
+            newChr= charactershifter(message[a], shiftvalue)
+            newStr += newChr
+   newStr= characterReturn(newStr, ynList)   
    return newStr
-print(encoder('GFABUM UE ZAF M DQMX BXMOQ', -27000))
+print(encoder(input("Insert the word of which you would like to encode:"), input("Enter the shift value of the encoder:")))
